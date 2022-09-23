42b9a3dfcfeaa0dd21cac2a332c84bc0578a5734
elmejrlui <elmejrlui@gmail.com>
2022-09-23 12:27:02 -0500

    feat(sections): refactor the mocks for the sections that requiere them

--- apps/siiges-app/pages/home/index.jsx
+++ apps/siiges-app/pages/home/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { HomePage, Layout } from '@siiges-ui/shared';
 
-export default function Home() {
+export default function HomeView() {
   return (
     <Layout title="Bienvenido">
       <HomePage />
