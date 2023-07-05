c0edddf6bd5f2f437cb2d28e27ec6ff6ba802b37
Luis Manuel <elmejorlui@gmail.com>
2023-07-05 13:24:04 -0600

    feat(inspecciones): vistas de jefe de inspeccion

--- packages/inspecciones/src/components/InspeccionesTable/index.jsx
+++ packages/inspecciones/src/components/InspeccionesTable/index.jsx
@@ -1,11 +1,24 @@
-import { Grid, Typography } from '@mui/material';
+import { Grid, TextField, Typography } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
 import { ButtonAdd } from '@siiges-ui/shared';
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { rows, columns } from '../Mocks/Inspecciones';
 
 export default function InspeccionesTable() {
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearchChange = (event) => {
+    setSearchText(event.target.value);
+  };
+
+  const filteredRows = searchText
+    ? rows.filter((row) => Object.values(row).some(
+      (value) => value
+            && value.toString().toLowerCase().includes(searchText.toLowerCase()),
+    ))
+    : rows;
+
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
@@ -16,10 +29,18 @@ export default function InspeccionesTable() {
           <ButtonAdd text="agregar" />
         </Grid>
       </Link>
+      <Grid item xs={9} sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <TextField
+          size="small"
+          value={searchText}
+          onChange={handleSearchChange}
+          placeholder="Filtrar..."
+        />
+      </Grid>
       <Grid item xs={12}>
         <div style={{ height: 400, width: '100%', marginTop: 15 }}>
           <DataGrid
-            rows={rows}
+            rows={filteredRows}
             columns={columns}
             pageSize={5}
             rowsPerPageOptions={[5]}
