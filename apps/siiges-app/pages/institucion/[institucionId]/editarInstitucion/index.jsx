ef80dce40c0dd35f520de4adf6ab12eb4ff16520
TaquitosDorados <62762581+TaquitosDorados@users.noreply.github.com>
2023-08-05 00:26:47 -0600

    feat(institucion): editar institucion

--- apps/siiges-app/pages/institucion/[institucionId]/editarInstitucion/index.jsx
+++ apps/siiges-app/pages/institucion/[institucionId]/editarInstitucion/index.jsx
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { ButtonsForm, Layout } from '@siiges-ui/shared';
+import { ButtonsForm, Layout, Context, SnackAlert } from '@siiges-ui/shared';
 import { EditInstitutionForm } from '@siiges-ui/instituciones';
 import getInstitucion from '../../utils/getInstitucion';
+import updateInstitucion from '../../utils/updateInstitucion';
 
 export default function editarInstitucion() {
+  const { session } = useContext(Context);
   const router = useRouter();
   const { institucion, loading } = getInstitucion();
+  const [noti, setNoti] = useState({ open: false, message: '', type: '' });
+  const [institucionForm, setInstitucionForm] = useState({});
+  const [errors, setErrors] = useState({});
+
+  const handleConfirm = () => {
+    updateInstitucion(institucionForm, errors, setNoti, router);
+  };
+
+  const handleClose = () => {
+    setNoti({ ...noti, open: false });
+  };
+
   return (
     <Layout title="Editar Institución">
-      {loading ? (
+      {loading ?
+       (
         <>
-          <EditInstitutionForm data={institucion.data} />
+          <EditInstitutionForm
+            data={institucion.data}
+            form={institucionForm}
+            setForm={setInstitucionForm}
+            setErrors={setErrors}
+          />
           <ButtonsForm
-            confirm={() => {}}
+            confirm={handleConfirm}
             cancel={() => {
               router.back();
             }}
+          />
+          <SnackAlert
+            open={noti.open}
+            close={handleClose}
+            type={noti.type}
+            mensaje={noti.message}
           />
         </>
       ) : (
