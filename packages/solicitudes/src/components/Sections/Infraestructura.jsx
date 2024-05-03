851a78f821f4e386dd6c7545d77ba0e74afca209
elmejorlui <elmejorlui@gmail.com>
2024-05-03 12:57:40 -0600

    feat(solicitudes): fixed anexos and infraestructuras

--- packages/solicitudes/src/components/Sections/Infraestructura.jsx
+++ packages/solicitudes/src/components/Sections/Infraestructura.jsx
@@ -47,7 +47,7 @@ export default function Infraestructura({ disabled, programaId }) {
       recursos: item.recursos,
       ubicacion: item.ubicacion,
       asignaturas: item.asignaturasInfraestructura?.map(
-        (asignaturaInfra) => asignaturaInfra.asignatura.nombre,
+        (asignaturaInfra) => asignaturaInfra.asignatura?.nombre,
       ),
     })),
     [infraestructuras],
