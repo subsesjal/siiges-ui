fefa3ccc86d436f59e52ed960482bbbec4a84711
LAU1980 <laura.jaramillo@jalisco.gob.mx>
2024-07-02 16:39:09 -0600

    mas acentos

--- packages/users/src/components/Usuarios/UsuariosTable/index.jsx
+++ packages/users/src/components/Usuarios/UsuariosTable/index.jsx
@@ -76,7 +76,7 @@ export default function UsuariosTable({ usuarios, session }) {
       />
       <DefaultModal open={modal} setOpen={hideModal} id={modalId} title="Eliminar Usuario">
         <Typography>
-          ¿Desea eliminar el usuari
+          ¿Desea eliminar el usuario
           {' '}
           {modalId && modalId.row.usuario}
           ?
