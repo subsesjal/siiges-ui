1a21784732bb8368250323e459426842ef6d9d1a
joe2141 <elsuperjoy@gmail.com>
2024-08-14 10:32:12 -0600

    correciones por roberto

--- packages/shared/src/components/Buttons/ButtonsForm.jsx
+++ packages/shared/src/components/Buttons/ButtonsForm.jsx
@@ -2,26 +2,26 @@ import React from 'react';
 import { Grid, Typography } from '@mui/material';
 import PropTypes from 'prop-types';
 import ButtonUnstyled from '@mui/base/ButtonUnstyled';
+
 import '../../styles/buttons/ButtonAdd.css';
 
-export default function UserForm({ confirm, cancel, confirmDisabled, confirmText = 'Confirmar' }) {
+export default function UserForm({ confirm, cancel, confirmDisabled, confirmText = 'Guardar' }) {
   return (
     <Grid container justifyContent="flex-end" spacing={2}>
       <Grid item>
         <ButtonUnstyled className="buttonAdd cancel" onClick={cancel}>
-          <Typography variant="body1">Cerrar</Typography>
+          <Typography variant="body1">Cancelar</Typography>
         </ButtonUnstyled>
       </Grid>
-      {!confirmDisabled && (
-        <Grid item>
-          <ButtonUnstyled
-            className="buttonAdd guardar"
-            onClick={confirm}
-          >
-            <Typography variant="body1">{confirmText}</Typography>
-          </ButtonUnstyled>
-        </Grid>
-      )}
+      <Grid item>
+        <ButtonUnstyled
+          className="buttonAdd guardar"
+          disabled={confirmDisabled}
+          onClick={confirm}
+        >
+          <Typography variant="body1">{confirmText}</Typography>
+        </ButtonUnstyled>
+      </Grid>
     </Grid>
   );
 }
@@ -29,6 +29,6 @@ export default function UserForm({ confirm, cancel, confirmDisabled, confirmText
 UserForm.propTypes = {
   confirm: PropTypes.func.isRequired,
   cancel: PropTypes.func.isRequired,
+  // eslint-disable-next-line react/require-default-props
   confirmDisabled: PropTypes.bool,
-  confirmText: PropTypes.string,
 };
