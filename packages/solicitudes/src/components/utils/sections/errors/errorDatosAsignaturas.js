f5f730d05c87b78bdfb5d6d7ccbc57cf2c56edb9
USUARIO <Lau1980@example.com>
2024-08-22 09:20:31 -0600

    errores rebase

--- packages/solicitudes/src/components/utils/sections/errors/errorDatosAsignaturas.js
+++ packages/solicitudes/src/components/utils/sections/errors/errorDatosAsignaturas.js
@@ -1,5 +1,8 @@
 const errorDatosAsignaturas = {
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> ca8a1ee (correcciones)
   gradoId: { message: '¡Seleccione un nivel!' },
   areaId: { message: '¡Seleccione un área!' },
   nombre: { message: '¡Nombre inválido!' },
@@ -7,6 +10,7 @@ const errorDatosAsignaturas = {
   creditos: { message: '¡Créditos inválidos!', isNumber: true },
   academia: { message: '¡Academia inválida!' },
   horasDocente: { message: '¡Horas docente inválidos!', isNumber: true },
+<<<<<<< HEAD
 =======
   gradoId: { message: 'Seleccione un nivel' },
   areaId: { message: 'Seleccione un área' },
@@ -16,6 +20,8 @@ const errorDatosAsignaturas = {
   academia: { message: 'Academia inválida' },
   horasDocente: { message: 'Horas docente inválidos', isNumber: true },
 >>>>>>> 310fb93 (correcciones de ortografía y archivos)
+=======
+>>>>>>> ca8a1ee (correcciones)
   horasIndependiente: {
     message: 'Horas independiente inválidos',
     isNumber: true,
