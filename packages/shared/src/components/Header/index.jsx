ec5bdcc769f6265f896923ce00328fa779869c9e
USUARIO <Lau1980@example.com>
2024-10-09 10:46:28 -0600

    ortografia28

--- packages/shared/src/components/Header/index.jsx
+++ packages/shared/src/components/Header/index.jsx
@@ -5,7 +5,7 @@ import '../../styles/Header/HeaderLogin.css';
 export default function Header() {
   return (
     <Typography component="h1" variant="h5" className="HeaderLogin">
-      Sistema integral de información para la gestión de Educación Superior
+      Sistema Integral de Información para la Gestión de Educación Superior
     </Typography>
   );
 }
