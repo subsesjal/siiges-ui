91a40f720afb680faba2d1d99af10c6b4857abef
elmejorlui <elmejorlui@gmail.com>
2024-10-16 16:58:13 -0600

    feat(equivalencias): conecting to backend

--- packages/revalidaciones/src/components/FormEquivalencias/Pages/DatosSolicitante.jsx
+++ packages/revalidaciones/src/components/FormEquivalencias/Pages/DatosSolicitante.jsx
@@ -3,13 +3,14 @@ import {
   Input, InputDate, Select, Subtitle,
 } from '@siiges-ui/shared';
 import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
 const apiKey = process.env.NEXT_PUBLIC_API_KEY;
 const domain = process.env.NEXT_PUBLIC_URL;
 
 const estados = [{ id: 14, nombre: 'Jalisco' }, { id: 15, nombre: 'Colima' }];
 
-export default function DatosSolicitante() {
+export default function DatosSolicitante({ form, handleOnChange }) {
   const [municipios, setMunicipios] = useState([]);
   const [estadoId, setEstadoId] = useState(null);
   const [municipiosDisabled, setMunicipiosDisabled] = useState(true);
@@ -26,7 +27,7 @@ export default function DatosSolicitante() {
           });
           const data = await response.json();
           const filteredMunicipios = data.data.filter(
-            (municipio) => municipio.estadoId === estadoId,
+            (municipio) => municipio.estadoId === parseInt(estadoId, 10),
           );
           setMunicipios(filteredMunicipios);
         } catch (error) {
@@ -42,6 +43,7 @@ export default function DatosSolicitante() {
     const selectedEstadoId = event.target.value;
     setEstadoId(selectedEstadoId);
     setMunicipiosDisabled(!selectedEstadoId);
+    handleOnChange(event);
   };
 
   return (
@@ -50,25 +52,49 @@ export default function DatosSolicitante() {
         <Subtitle>Trámite de Equivalencia</Subtitle>
       </Grid>
       <Grid item xs={3}>
-        <Select title="Tipo de Solicitud" options={[]} name="tipoSolicitud" />
+        <Select
+          title="Tipo de Solicitud"
+          options={[]}
+          name="tipoSolicitud"
+          onChange={handleOnChange}
+        />
       </Grid>
       <Grid item xs={3}>
-        <Select title="Grado Académico" options={[]} name="gradoAcademico" />
+        <Select
+          title="Grado Académico"
+          options={[]}
+          name="gradoAcademico"
+          onChange={handleOnChange}
+        />
       </Grid>
       <Grid item xs={12}>
         <Subtitle>Datos del Solicitante</Subtitle>
       </Grid>
       <Grid item xs={3}>
-        <Input id="curp" label="CURP" name="curp" />
+        <Input
+          id="curp"
+          label="CURP"
+          name="curp"
+          value={form.curp || ''}
+          onChange={handleOnChange}
+        />
       </Grid>
       <Grid item xs={3}>
-        <Input id="nombre" label="Nombre" name="nombre" />
+        <Input
+          id="nombre"
+          label="Nombre"
+          name="nombre"
+          value={form.nombre || ''}
+          onChange={handleOnChange}
+        />
       </Grid>
       <Grid item xs={3}>
         <Input
           id="primerApellido"
           label="Primer Apellido"
           name="primerApellido"
+          value={form.primerApellido || ''}
+          onChange={handleOnChange}
         />
       </Grid>
       <Grid item xs={3}>
@@ -76,32 +102,54 @@ export default function DatosSolicitante() {
           id="segundoApellido"
           label="Segundo Apellido"
           name="segundoApellido"
+          value={form.segundoApellido || ''}
+          onChange={handleOnChange}
         />
       </Grid>
       <Grid item xs={3}>
         <InputDate
           label="Fecha de Nacimiento"
           name="fechaNacimiento"
-          onChange={() => {}}
+          value={form.fechaNacimiento || ''}
+          onChange={handleOnChange}
         />
       </Grid>
       <Grid item xs={12}>
         <Subtitle>Dirección</Subtitle>
       </Grid>
       <Grid item xs={9}>
-        <Input name="calle" id="calle" label="Calle" />
+        <Input
+          name="calle"
+          id="calle"
+          label="Calle"
+          value={form.calle || ''}
+          onChange={handleOnChange}
+        />
       </Grid>
       <Grid item xs={3}>
-        <Input name="numero" id="numero" label="Número" />
+        <Input
+          name="numero"
+          id="numero"
+          label="Número"
+          value={form.numero || ''}
+          onChange={handleOnChange}
+        />
       </Grid>
       <Grid item xs={3}>
-        <Input name="colonia" id="colonia" label="Colonia" />
+        <Input
+          name="colonia"
+          id="colonia"
+          label="Colonia"
+          value={form.colonia || ''}
+          onChange={handleOnChange}
+        />
       </Grid>
       <Grid item xs={3}>
         <Select
           title="Estados"
           name="estadoId"
           options={estados}
+          value={form.estadoId || ''}
           onChange={handleEstadoChange}
         />
       </Grid>
@@ -111,20 +159,59 @@ export default function DatosSolicitante() {
           name="municipioId"
           options={municipios}
           disabled={municipiosDisabled}
+          value={form.municipioId || ''}
+          onChange={handleOnChange}
         />
       </Grid>
       <Grid item xs={3}>
-        <Input name="codigoPostal" id="codigoPostal" label="Código Postal" />
+        <Input
+          name="codigoPostal"
+          id="codigoPostal"
+          label="Código Postal"
+          value={form.codigoPostal || ''}
+          onChange={handleOnChange}
+        />
       </Grid>
       <Grid item xs={12}>
         <Subtitle>Contacto</Subtitle>
       </Grid>
       <Grid item xs={3}>
-        <Input name="correo" id="correo" label="Correo de Contacto" />
+        <Input
+          name="correo"
+          id="correo"
+          label="Correo de Contacto"
+          value={form.correo || ''}
+          onChange={handleOnChange}
+        />
       </Grid>
       <Grid item xs={3}>
-        <Input name="telefono" id="telefono" label="Teléfono de Contacto" />
+        <Input
+          name="telefono"
+          id="telefono"
+          label="Teléfono de Contacto"
+          value={form.telefono || ''}
+          onChange={handleOnChange}
+        />
       </Grid>
     </Grid>
   );
 }
+
+DatosSolicitante.propTypes = {
+  form: PropTypes.shape({
+    curp: PropTypes.string,
+    nombre: PropTypes.string,
+    primerApellido: PropTypes.string,
+    segundoApellido: PropTypes.string,
+    fechaNacimiento: PropTypes.string,
+    calle: PropTypes.string,
+    numero: PropTypes.string,
+    colonia: PropTypes.string,
+    estadoId: PropTypes.string,
+    municipioId: PropTypes.string,
+    codigoPostal: PropTypes.string,
+    correo: PropTypes.string,
+    telefono: PropTypes.string,
+  }).isRequired,
+  handleOnChange: PropTypes.func.isRequired,
+};
