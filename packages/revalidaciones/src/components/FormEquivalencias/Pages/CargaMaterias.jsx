a1f73f1c84f7c8187090fe0216bf6fd619862b9c
elmejorlui <elmejorlui@gmail.com>
2024-10-31 15:47:54 -0600

    feat(equivalencias): fixed logic on the submit

--- packages/revalidaciones/src/components/FormEquivalencias/Pages/CargaMaterias.jsx
+++ packages/revalidaciones/src/components/FormEquivalencias/Pages/CargaMaterias.jsx
@@ -3,18 +3,25 @@ import { InputFile, fileToFormData } from '@siiges-ui/shared';
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function CargaMaterias({ setFilesData }) {
+export default function CargaMaterias({ filesData, setFilesData }) {
   const handleFileChange = async (files, name) => {
     try {
       const formData = await fileToFormData(files[0]);
       setFilesData((prevFilesData) => ({
         ...prevFilesData,
-        [name]: formData,
+        [name]: { formData, url: URL.createObjectURL(files[0]) },
       }));
     } catch (error) {
       console.error('Error al procesar el archivo:', error);
     }
   };
+
+  const formatUrl = (fileKey) => {
+    const fileUrl = filesData[fileKey]?.url || '';
+    return fileUrl.startsWith('blob:') ? `${fileUrl.replace('blob:', '')}` : fileUrl;
+  };
+
+  const isFileUploaded = (fileKey) => !!filesData[fileKey]?.formData;
 
   return (
     <Grid container spacing={1}>
@@ -24,7 +31,9 @@ export default function CargaMaterias({ setFilesData }) {
           id="curp"
           tipoDocumento="CURP"
           tipoEntidad="EQUIVALENCIAS"
+          url={formatUrl('CURP')}
           onChange={(files) => handleFileChange(files, 'CURP')}
+          isUploaded={isFileUploaded('CURP')}
         />
       </Grid>
       <Grid item xs={6}>
@@ -33,7 +42,9 @@ export default function CargaMaterias({ setFilesData }) {
           id="identificacionOficial"
           tipoDocumento="IDENTIFICACION_OFICIAL"
           tipoEntidad="EQUIVALENCIAS"
+          url={formatUrl('IDENTIFICACION_OFICIAL')}
           onChange={(files) => handleFileChange(files, 'IDENTIFICACION_OFICIAL')}
+          isUploaded={isFileUploaded('IDENTIFICACION_OFICIAL')}
         />
       </Grid>
       <Grid item xs={6}>
@@ -42,7 +53,9 @@ export default function CargaMaterias({ setFilesData }) {
           id="actaNacimiento"
           tipoDocumento="ACTA_NACIMIENTO"
           tipoEntidad="EQUIVALENCIAS"
+          url={formatUrl('ACTA_NACIMIENTO')}
           onChange={(files) => handleFileChange(files, 'ACTA_NACIMIENTO')}
+          isUploaded={isFileUploaded('ACTA_NACIMIENTO')}
         />
       </Grid>
       <Grid item xs={6}>
@@ -51,7 +64,9 @@ export default function CargaMaterias({ setFilesData }) {
           id="copiaResolucion"
           tipoDocumento="COPIA_RESOLUCION"
           tipoEntidad="EQUIVALENCIAS"
+          url={formatUrl('COPIA_RESOLUCION')}
           onChange={(files) => handleFileChange(files, 'COPIA_RESOLUCION')}
+          isUploaded={isFileUploaded('COPIA_RESOLUCION')}
         />
       </Grid>
       <Grid item xs={6}>
@@ -60,7 +75,9 @@ export default function CargaMaterias({ setFilesData }) {
           id="certificadoParcialTotal"
           tipoDocumento="CERTIFICADO_PARCIAL_TOTAL"
           tipoEntidad="EQUIVALENCIAS"
+          url={formatUrl('CERTIFICADO_PARCIAL_TOTAL')}
           onChange={(files) => handleFileChange(files, 'CERTIFICADO_PARCIAL_TOTAL')}
+          isUploaded={isFileUploaded('CERTIFICADO_PARCIAL_TOTAL')}
         />
       </Grid>
       <Grid item xs={6}>
@@ -69,7 +86,9 @@ export default function CargaMaterias({ setFilesData }) {
           id="antecedenteAcademico"
           tipoDocumento="ANTECEDENTE_ACADEMICO"
           tipoEntidad="EQUIVALENCIAS"
+          url={formatUrl('ANTECEDENTE_ACADEMICO')}
           onChange={(files) => handleFileChange(files, 'ANTECEDENTE_ACADEMICO')}
+          isUploaded={isFileUploaded('ANTECEDENTE_ACADEMICO')}
         />
       </Grid>
       <Grid item xs={6}>
@@ -78,7 +97,9 @@ export default function CargaMaterias({ setFilesData }) {
           id="programaEstudioAutorizado"
           tipoDocumento="PROGRAMA_ESTUDIO_AUTORIZADO"
           tipoEntidad="EQUIVALENCIAS"
+          url={formatUrl('PROGRAMA_ESTUDIO_AUTORIZADO')}
           onChange={(files) => handleFileChange(files, 'PROGRAMA_ESTUDIO_AUTORIZADO')}
+          isUploaded={isFileUploaded('PROGRAMA_ESTUDIO_AUTORIZADO')}
         />
       </Grid>
       <Grid item xs={6}>
@@ -87,7 +108,9 @@ export default function CargaMaterias({ setFilesData }) {
           id="propuestaEquivalencia"
           tipoDocumento="PROPUESTA_EQUIVALENCIA"
           tipoEntidad="EQUIVALENCIAS"
+          url={formatUrl('PROPUESTA_EQUIVALENCIA')}
           onChange={(files) => handleFileChange(files, 'PROPUESTA_EQUIVALENCIA')}
+          isUploaded={isFileUploaded('PROPUESTA_EQUIVALENCIA')}
         />
       </Grid>
       <Grid item xs={6}>
@@ -96,7 +119,9 @@ export default function CargaMaterias({ setFilesData }) {
           id="pagoEquivalencia"
           tipoDocumento="PAGO_EQUIVALENCIA"
           tipoEntidad="EQUIVALENCIAS"
+          url={formatUrl('PAGO_EQUIVALENCIA')}
           onChange={(files) => handleFileChange(files, 'PAGO_EQUIVALENCIA')}
+          isUploaded={isFileUploaded('PAGO_EQUIVALENCIA')}
         />
       </Grid>
     </Grid>
@@ -104,5 +129,43 @@ export default function CargaMaterias({ setFilesData }) {
 }
 
 CargaMaterias.propTypes = {
+  filesData: PropTypes.shape({
+    CURP: PropTypes.shape({
+      formData: PropTypes.instanceOf(FormData),
+      url: PropTypes.string,
+    }),
+    IDENTIFICACION_OFICIAL: PropTypes.shape({
+      formData: PropTypes.instanceOf(FormData),
+      url: PropTypes.string,
+    }),
+    ACTA_NACIMIENTO: PropTypes.shape({
+      formData: PropTypes.instanceOf(FormData),
+      url: PropTypes.string,
+    }),
+    COPIA_RESOLUCION: PropTypes.shape({
+      formData: PropTypes.instanceOf(FormData),
+      url: PropTypes.string,
+    }),
+    CERTIFICADO_PARCIAL_TOTAL: PropTypes.shape({
+      formData: PropTypes.instanceOf(FormData),
+      url: PropTypes.string,
+    }),
+    ANTECEDENTE_ACADEMICO: PropTypes.shape({
+      formData: PropTypes.instanceOf(FormData),
+      url: PropTypes.string,
+    }),
+    PROGRAMA_ESTUDIO_AUTORIZADO: PropTypes.shape({
+      formData: PropTypes.instanceOf(FormData),
+      url: PropTypes.string,
+    }),
+    PROPUESTA_EQUIVALENCIA: PropTypes.shape({
+      formData: PropTypes.instanceOf(FormData),
+      url: PropTypes.string,
+    }),
+    PAGO_EQUIVALENCIA: PropTypes.shape({
+      formData: PropTypes.instanceOf(FormData),
+      url: PropTypes.string,
+    }),
+  }).isRequired,
   setFilesData: PropTypes.func.isRequired,
 };
