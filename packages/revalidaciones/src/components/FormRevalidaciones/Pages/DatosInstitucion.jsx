802976541ae9eee06f59ca9150279f5bb7cfad01
elmejorlui <elmejorlui@gmail.com>
2024-12-03 16:05:55 -0600

    feat(revalidaciones): corrected views revalidaciones

--- packages/revalidaciones/src/components/FormRevalidaciones/Pages/DatosInstitucion.jsx
+++ packages/revalidaciones/src/components/FormRevalidaciones/Pages/DatosInstitucion.jsx
@@ -1,102 +1,217 @@
 import { Grid } from '@mui/material';
-import { Input, Select, Subtitle } from '@siiges-ui/shared';
-import React from 'react';
+import {
+  getData, Input, Select, Subtitle,
+} from '@siiges-ui/shared';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
-export default function DatosInstitucion() {
+const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+const domain = process.env.NEXT_PUBLIC_URL;
+
+export default function DatosInstitucion({ form, handleOnChange, estados }) {
+  const [tipoInstituciones, setTipoInstituciones] = useState([]);
+  const [programas, setProgramas] = useState([]);
+  const [instituciones, setInstituciones] = useState([]);
+  const [grados, setGrados] = useState([]);
+  const [tipoInstitucionId, setTipoInstitucionId] = useState(form.institucionDestino?.tipoInstitucionId || '');
+
+  useEffect(() => {
+    const fetchTipoInstituciones = async () => {
+      try {
+        const response = await fetch(`${domain}/api/v1/public/instituciones/tipoInstituciones`, {
+          headers: {
+            api_key: apiKey,
+            'Content-Type': 'application/json',
+          },
+        });
+        const data = await response.json();
+        setTipoInstituciones(data.data);
+      } catch (error) {
+        console.error('Error fetching tipo de instituciones:', error);
+      }
+    };
+
+    setGrados(getData({ endpoint: '/public/grados/' }));
+    fetchTipoInstituciones();
+  }, []);
+
+  const fetchInstituciones = async () => {
+    try {
+      const response = await fetch(
+        `${domain}/api/v1/public/instituciones?tipoInstitucionId=${tipoInstitucionId}`,
+        {
+          headers: {
+            api_key: apiKey,
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+      const data = await response.json();
+      setInstituciones(data.data);
+    } catch (error) {
+      console.error('Error fetching instituciones:', error);
+    }
+  };
+
+  const fetchProgramas = async (acuerdoRvoe) => {
+    try {
+      const response = await fetch(
+        `${domain}/api/v1/public/programas?acuerdoRvoe=${acuerdoRvoe}`,
+        {
+          headers: {
+            api_key: apiKey,
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+      const data = await response.json();
+      setProgramas(data.data);
+    } catch (error) {
+      console.error('Error fetching Programas:', error);
+    }
+  };
+
+  useEffect(() => {
+    if (tipoInstitucionId === 1) {
+      fetchInstituciones();
+    }
+  }, [tipoInstitucionId]);
+
+  const handleTipoInstitucionChange = (event) => {
+    const selectedTipoInstitucionId = event.target.value;
+    setTipoInstitucionId(selectedTipoInstitucionId);
+    handleOnChange(event, ['interesado', 'institucionDestino']);
+  };
+
+  const handleRvoeOnBlur = (event) => {
+    const acuerdoRvoe = event.target.value;
+    if (tipoInstitucionId === 1) {
+      fetchProgramas(acuerdoRvoe);
+    }
+    handleOnChange(event, ['interesado', 'institucionDestino']);
+  };
+
   return (
     <Grid container spacing={1}>
       <Grid item xs={12}>
-        <Subtitle>
-          Datos de la Institución de procedencia en el extranjero
-        </Subtitle>
+        <Subtitle>Datos de la Institución de procedencia</Subtitle>
       </Grid>
       <Grid item xs={9}>
         <Input
           id="nombreInstitucion"
           label="Nombre de la Institución"
-          name="nombreInstitucion"
+          name="nombre"
+          value={form.institucionProcedencia?.nombre || ''}
+          onChange={(e) => handleOnChange(e, ['interesado', 'institucionProcedencia'])}
         />
       </Grid>
       <Grid item xs={3}>
-        <Select title="Estado" options={[]} name="estado" />
+        <Select
+          title="Grado Academico procedente"
+          options={grados}
+          name="gradoAcademicoProcedente"
+          value={form.institucionProcedencia?.gradoAcademicoProcedente || ''}
+          onChange={(e) => handleOnChange(e, ['interesado', 'institucionProcedencia'])}
+        />
       </Grid>
       <Grid item xs={9}>
         <Input
           id="nombreCarrera"
           label="Nombre de la Carrera"
           name="nombreCarrera"
+          value={form.institucionProcedencia?.nombreCarrera || ''}
+          onChange={(e) => handleOnChange(e, ['interesado', 'institucionProcedencia'])}
         />
       </Grid>
       <Grid item xs={3}>
         <Select
-          title="Grado Académico Procedente"
-          options={[]}
-          name="gradoAcademicoProcedente"
+          title="Pais"
+          options={estados}
+          name="paisId"
+          value={form.institucionProcedencia?.paisId || ''}
+          onChange={(e) => handleOnChange(e, ['interesado', 'institucionProcedencia'])}
         />
       </Grid>
-      <Grid item xs={6}>
-        <Input
-          id="anoFinalizacion"
-          name="anoFinalizacion"
-          label="Año de Finalización de Estudios"
-        />
-      </Grid>
-      <Grid item xs={6}>
-        <Input
-          id="anoInicio"
-          name="anoInicio"
-          label="Año de Inicio de realización de Estudios"
-        />
-      </Grid>
-      <Grid item xs={4}>
-        <Select title="Estado" name="estado" options={[]} />
-      </Grid>
-      <Grid item xs={4}>
-        <Select title="Ciudad" name="ciudad" options={[]} />
-      </Grid>
-      <Grid item xs={4}>
-        <Select title="País" name="pais" options={[]} />
-      </Grid>
-      <Grid item xs={12}>
-        <Subtitle>
-          En caso de no contar con apostilla proporcionar datos de la
-          universidad para verificación de autenticidad
-        </Subtitle>
-      </Grid>
-      <Grid item xs={3}>
-        <Input
-          id="telefonoInstitucion"
-          name="telefonoInstitucion"
-          label="Teléfono de institución"
-        />
-      </Grid>
-      <Grid item xs={9}>
-        <Input
-          id="paginaWeb"
-          name="paginaWeb"
-          label="Página web de la institución"
-        />
-      </Grid>
-      <Grid item xs={6}>
-        <Input
-          id="correoInstitucion"
-          name="correoInstitucion"
-          label="Correo de institución"
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <Subtitle>Deseo revalidar mis estudios como</Subtitle>
-      </Grid>
-      <Grid item xs={4}>
-        <Select
-          title="Grado académico destino"
-          name="gradoAcademicoDestino"
-          options={[]}
-        />
-      </Grid>
-      <Grid item xs={8}>
-        <Input id="planEstudios" name="planEstudios" label="Plan de estudios" />
-      </Grid>
+      {form.tipoTramiteId === 1 && (
+      <>
+        <Grid item xs={12}>
+          <Subtitle>Datos de la Institución de destino</Subtitle>
+        </Grid>
+        <Grid item xs={3}>
+          <Select
+            title="Tipo de Institución"
+            name="tipoInstitucionId"
+            options={tipoInstituciones}
+            value={tipoInstitucionId}
+            onChange={handleTipoInstitucionChange}
+          />
+        </Grid>
+        <Grid item xs={9}>
+          {tipoInstitucionId === 1 ? (
+            <Select
+              title="Instituciones"
+              options={instituciones}
+              name="programaId"
+              value={form.institucionDestino?.programaId || ''}
+              onChange={(e) => handleOnChange(e, ['interesado', 'institucionDestino'])}
+            />
+          ) : (
+            <Input
+              id="institucionNombre"
+              label="Instituciones de Educación Superior"
+              name="nombre"
+              value={form.institucionDestino?.nombre || ''}
+              onChange={(e) => handleOnChange(e, ['interesado', 'institucionDestino'])}
+            />
+          )}
+        </Grid>
+        <Grid item xs={3}>
+          <Input
+            id="rvoe"
+            label="RVOE"
+            name="acuerdoRvoe"
+            value={form.institucionDestino?.acuerdoRvoe || ''}
+            onblur={handleRvoeOnBlur}
+          />
+        </Grid>
+        <Grid item xs={9}>
+          <Input
+            id="nombreCarreraDestino"
+            label="Nombre de la Carrera (Destino)"
+            name="nombreCarrera"
+            value={programas.nombre || ''}
+            onChange={(e) => handleOnChange(e, ['interesado', 'institucionDestino'])}
+            disabled={tipoInstitucionId === 1}
+          />
+        </Grid>
+      </>
+      )}
     </Grid>
   );
 }
+
+DatosInstitucion.propTypes = {
+  form: PropTypes.shape({
+    tipoTramiteId: PropTypes.number,
+    institucionProcedencia: PropTypes.shape({
+      nombre: PropTypes.string,
+      paisId: PropTypes.string,
+      nombreCarrera: PropTypes.string,
+      gradoAcademicoProcedente: PropTypes.string,
+    }),
+    institucionDestino: PropTypes.shape({
+      tipoInstitucionId: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+      programaId: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+      nombre: PropTypes.string,
+      acuerdoRvoe: PropTypes.string,
+      nombreCarrera: PropTypes.string,
+    }),
+  }).isRequired,
+  handleOnChange: PropTypes.func.isRequired,
+  estados: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+      nombre: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
--- packages/revalidaciones/src/components/FormRevalidaciones/index.jsx
+++ packages/revalidaciones/src/components/FormRevalidaciones/index.jsx
@@ -1,14 +1,89 @@
 import { Grid } from '@mui/material';
-import { PositionDisplay } from '@siiges-ui/shared';
-import React, { useState } from 'react';
+import { Context, PositionDisplay } from '@siiges-ui/shared';
+import React, { useContext, useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import NavigationButtons from '../../utils/NavigationButtons';
 import DatosSolicitante from './Pages/DatosSolicitante';
 import DatosInstitucion from './Pages/DatosInstitucion';
 import CargaMaterias from './Pages/CargaMaterias';
+import CargaMateriasEquivalentes from '../FormEquivalencias/Pages/CargaMateriasEquivalentes';
+
+const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+const domain = process.env.NEXT_PUBLIC_URL;
 
 export default function FormRevalidaciones() {
   const [currentPosition, setCurrentPosition] = useState(1);
-  const totalPositions = 3;
+  const [totalPositions, setTotalPositions] = useState(4);
+  const { setNoti } = useContext(Context);
+  const router = useRouter();
+  const [filesData, setFilesData] = useState({});
+  const [estados, setEstados] = useState([]);
+  const [form, setForm] = useState({
+    tipoTramiteId: null,
+    estatusSolicitudRevEquivId: 2,
+    fecha: new Date().toISOString().split('T')[0],
+    interesado: {
+      persona: {
+        domicilio: {
+          calle: '',
+          numeroExterior: '',
+          numeroInterior: '',
+          colonia: '',
+          codigoPostal: '',
+          municipioId: '',
+          estadoId: '',
+        },
+        nombre: '',
+        apellidoPaterno: '',
+        apellidoMaterno: '',
+        telefono: '',
+        curp: '',
+        correoPrincipal: '',
+      },
+      institucionProcedencia: {
+        tipoInstitucionId: 1,
+        nombre: '',
+        estadoId: '',
+        nombreCarrera: '',
+      },
+      institucionDestino: {
+        tipoInstitucionId: '',
+        programaId: 1,
+        nombre: '',
+        acuerdoRvoe: '',
+        nombreCarrera: '',
+      },
+    },
+    asignaturaAntecedente: [],
+    asignaturaEquivalente: [],
+  });
+
+  useEffect(() => {
+    if (form.tipoTramiteId === 1) {
+      setTotalPositions(4);
+    } else {
+      setTotalPositions(3);
+    }
+  }, [form.tipoTramiteId]);
+
+  useEffect(() => {
+    const fetchEstados = async () => {
+      try {
+        const response = await fetch(`${domain}/api/v1/public/estados/`, {
+          headers: {
+            api_key: apiKey,
+            'Content-Type': 'application/json',
+          },
+        });
+        const data = await response.json();
+        setEstados(data.data);
+      } catch (error) {
+        console.error('¡Error al buscar Estados!:', error);
+      }
+    };
+
+    fetchEstados();
+  }, []);
 
   const handleNext = () => {
     if (currentPosition < totalPositions) {
@@ -22,14 +97,81 @@ export default function FormRevalidaciones() {
     }
   };
 
+  const handleOnChange = (event, path = []) => {
+    const { name, value } = event.target;
+
+    setForm((prevForm) => {
+      const updateNestedValue = (obj, nestedPath) => {
+        if (nestedPath.length === 0) {
+          return { ...obj, [name]: value };
+        }
+
+        const [firstKey, ...restPath] = nestedPath;
+        return {
+          ...obj,
+          [firstKey]: updateNestedValue(obj[firstKey] || {}, restPath),
+        };
+      };
+
+      return updateNestedValue(prevForm, path);
+    });
+  };
+
+  const handleOnSubmit = async () => {
+    const formData = new FormData();
+
+    Object.keys(filesData).forEach((key) => {
+      const archivoAdjunto = filesData[key].formData.get('archivoAdjunto');
+      if (archivoAdjunto) {
+        formData.append(key, archivoAdjunto);
+      } else {
+        console.error(`No se encontró el archivo para ${key}`);
+      }
+    });
+
+    formData.append('DATA', JSON.stringify(form));
+
+    try {
+      const response = await fetch(`${domain}/api/v1/public/solicitudesRevEquiv/`, {
+        method: 'POST',
+        headers: {
+          api_key: apiKey,
+        },
+        body: formData,
+      });
+
+      if (!response.ok) {
+        throw new Error('¡Error al enviar el formulario!');
+      }
+
+      setNoti({
+        open: true,
+        message: 'Se envió la solicitud con éxito',
+        type: 'success',
+      });
+      router.reload();
+    } catch (error) {
+      console.error('¡Error al enviar el formulario!', error);
+      setNoti({
+        open: true,
+        message: '¡Ocurrió un error al enviar la solicitud, intente de nuevo!',
+        type: 'error',
+      });
+    }
+  };
+
   const renderCurrentPage = () => {
     switch (currentPosition) {
       case 1:
-        return <DatosSolicitante />;
+        return <DatosSolicitante form={form} handleOnChange={handleOnChange} estados={estados} />;
       case 2:
-        return <DatosInstitucion />;
+        return <DatosInstitucion form={form} handleOnChange={handleOnChange} estados={estados} />;
       case 3:
-        return <CargaMaterias />;
+        return <CargaMaterias filesData={filesData} setFilesData={setFilesData} />;
+      case 4:
+        return form.tipoTramiteId === 1 ? (
+          <CargaMateriasEquivalentes form={form} handleOnChange={handleOnChange} />
+        ) : null;
       default:
         return null;
     }
@@ -49,6 +191,7 @@ export default function FormRevalidaciones() {
           totalPositions={totalPositions}
           onNext={handleNext}
           onPrevious={handlePrevious}
+          handleOnSubmit={handleOnSubmit}
         />
       </Grid>
     </Grid>
