8922c8555059a50ef6ae8365c356bfcb286e29d6
cetherdev <robert.hct@gmail.com>
2025-03-10 19:54:44 -0600

    change loading for context

--- apps/siiges-app/pages/solicitudesBecas/crear/index.jsx
+++ apps/siiges-app/pages/solicitudesBecas/crear/index.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
-import { Layout } from '@siiges-ui/shared';
+import React, { useContext } from 'react';
+import { Layout, Context } from '@siiges-ui/shared';
 import { BecasComponents } from '@siiges-ui/serviciosescolares';
 
 export default function AgregarSolicitudBecas() {
-  const [loading, setLoading] = useState(false);
+  const { loading, setLoading } = useContext(Context);
+
   return (
     <Layout title="Agregar Solicitud de Becas" loading={loading}>
       <BecasComponents.SolicitudesBecasBox setLoading={setLoading} type="crear" />
