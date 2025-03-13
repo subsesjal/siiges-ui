97a217106e5bd668ceefa4bb42c6ecbdf0182966
elmejorlui <elmejorlui@gmail.com>
2025-03-12 20:57:43 -0600

    feat(becas): added observaciones input

--- packages/serviciosEscolares/src/Components/BecasServiciosSociales/Becas/SolicitudesBecasBox/ButtonsReviewBox/index.jsx
+++ packages/serviciosEscolares/src/Components/BecasServiciosSociales/Becas/SolicitudesBecasBox/ButtonsReviewBox/index.jsx
@@ -1,31 +1,64 @@
 import { Grid } from '@mui/material';
-import { ButtonsForm, ButtonSimple, Context } from '@siiges-ui/shared';
+import {
+  ButtonsForm, ButtonSimple, Context, Input,
+} from '@siiges-ui/shared';
 import PropTypes from 'prop-types';
-import React, { useContext, useCallback } from 'react';
+import React, { useContext, useCallback, useState } from 'react';
 import { handleUpdateSolicitud } from '../../utils';
 
 export default function ButtonsReviewBox({ router, solicitudId }) {
   const { setNoti, setLoading } = useContext(Context);
+  const [form, setForm] = useState({
+    observaciones: '',
+    estatusSolicitudBecaId: null,
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setForm((prevForm) => ({
+      ...prevForm,
+      [name]: value,
+    }));
+  };
 
   const handleConfirm = useCallback(() => {
-    handleUpdateSolicitud(setNoti, setLoading, { estatusSolicitudBecaId: 3 }, solicitudId);
+    const payload = {
+      estatusSolicitudBecaId: 3,
+    };
+    handleUpdateSolicitud(setNoti, setLoading, payload, solicitudId);
     router.back();
-  }, [setNoti, setLoading, solicitudId]);
+  }, [setNoti, setLoading, solicitudId, form.observaciones, router]);
 
   const handleCancel = useCallback(() => {
-    handleUpdateSolicitud(setNoti, setLoading, { estatusSolicitudBecaId: 4 }, solicitudId);
+    const payload = {
+      estatusSolicitudBecaId: 4,
+      observaciones: form.observaciones,
+    };
+    handleUpdateSolicitud(setNoti, setLoading, payload, solicitudId);
     router.back();
-  }, [setNoti, setLoading, solicitudId]);
+  }, [setNoti, setLoading, solicitudId, form.observaciones, router]);
 
   return (
     <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <Input
+          label="Observaciones"
+          id="observaciones"
+          name="observaciones"
+          value={form.observaciones}
+          onChange={handleChange}
+          multiline
+          rows={4}
+        />
+      </Grid>
       <Grid item xs={6}>
         <ButtonSimple text="Regresar" design="enviar" onClick={() => router.back()} />
       </Grid>
       <Grid item xs={6}>
         <ButtonsForm
           confirm={handleConfirm}
-          confirmText="Enviar"
+          confirmText="Procesar Solicitud"
+          cancelText="Enviar Observaciones"
           cancel={handleCancel}
         />
       </Grid>
