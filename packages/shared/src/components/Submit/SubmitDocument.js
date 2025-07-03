66427c3d26940e92c7e012cca3f29e7fc4f26cee
Arely Rizo <arelyruiz50700@gmail.com>
2025-07-03 16:39:54 -0600

    Notificacion de errores

--- packages/shared/src/components/Submit/SubmitDocument.js
+++ packages/shared/src/components/Submit/SubmitDocument.js
@@ -1,34 +1,26 @@
-export default async function SubmitDocument(formData, setUrl, setNoti) {
-  try {
-    const apikey = process.env.NEXT_PUBLIC_API_KEY;
-    const url = process.env.NEXT_PUBLIC_URL;
+export default async function SubmitDocument(formData, setUrl) {
+  const apikey = process.env.NEXT_PUBLIC_API_KEY;
+  const url = process.env.NEXT_PUBLIC_URL;
 
-    if (!formData) {
-      throw new Error('¡No se proporcionaron datos de formulario!.');
-    }
+  if (!formData) {
+    throw new Error('¡No se proporcionaron datos de formulario!.');
+  }
 
-    const response = await fetch(`${url}/api/v1/files/`, {
-      method: 'POST',
-      headers: { api_key: apikey },
-      body: formData,
-    });
+  const response = await fetch(`${url}/api/v1/files/`, {
+    method: 'POST',
+    headers: { api_key: apikey },
+    body: formData,
+  });
 
-    if (!response.ok) {
-      throw new Error(`¡No se pudo cargar el documento!: ${response.statusText}`);
-    }
+  if (!response.ok) {
+    throw new Error(`¡No se pudo cargar el documento!: ${response.statusText}`);
+  }
 
-    const data = await response.json();
+  const data = await response.json();
 
-    if (data && data.ubicacion) {
-      setUrl(data.ubicacion);
-    } else {
-      throw new Error('¡No se encontró "ubicación" en la respuesta.!');
-    }
-  } catch (err) {
-    setNoti({
-      open: true,
-      message: err.message || '¡Algo salió mal, revise su documento!',
-      type: 'error',
-    });
+  if (data && data.ubicacion) {
+    setUrl(data.ubicacion);
+  } else {
+    throw new Error('¡No se encontró "ubicación" en la respuesta.!');
   }
 }
