f07e9e9d10f0f0522473fbe2e2981d04585cf272
elmejorlui <elmejorlui@gmail.com>
2025-07-14 15:09:43 -0600

    feat(login): removed relevant message

--- apps/siiges-app/pages/autenticacion/preRegistro/index.jsx
+++ apps/siiges-app/pages/autenticacion/preRegistro/index.jsx
@@ -1,67 +1,15 @@
 import React, { useEffect } from 'react';
-import { Typography } from '@mui/material';
 import { LoginLayout } from '@siiges-ui/shared';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import { useRouter } from 'next/router';
 
 function SignUpPage() {
   const router = useRouter();
 
   useEffect(() => {
-    const timeout = setTimeout(() => {
-      router.push('/');
-    }, 500);
-
-    return () => clearTimeout(timeout);
+    router.push('/');
   }, [router]);
   return (
-    <LoginLayout>
-      <Box
-        sx={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          '& > :not(style)': {
-            mt: 3,
-            py: 3,
-            px: 3,
-            width: '320px',
-          },
-        }}
-        justifyContent="center"
-      >
-        <Paper
-          elevation={5}
-          sx={{
-            fontSize: 12,
-            '&:hover': {
-              boxShadow: '15',
-            },
-            backgroundColor: 'rgb(255, 255, 255, 0.75)',
-          }}
-        >
-          <Box
-            component="img"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_KiSKVaMTHgwDXl1nG-iJig77m_RkeVp2jQ&s"
-            alt="Nada que ver"
-            sx={{
-              width: '100%',
-              maxHeight: 180,
-              objectFit: 'contain',
-              mb: 2,
-              borderRadius: 2,
-            }}
-          />
-          <Typography
-            component="h1"
-            variant="h5"
-            sx={{ textAlign: 'center', color: 'black' }}
-          >
-            Shhhh. Aqui no hay nada que ver.
-          </Typography>
-        </Paper>
-      </Box>
-    </LoginLayout>
+    <LoginLayout />
   );
 }
 
