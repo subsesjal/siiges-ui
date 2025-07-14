954432149305699808ba4cb4e5bae8d31d49ebbd
elmejorlui <elmejorlui@gmail.com>
2025-07-14 14:17:54 -0600

    feat(login): removed preregistro button

--- packages/shared/src/components/Navbar/index.jsx
+++ packages/shared/src/components/Navbar/index.jsx
@@ -3,9 +3,7 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import Link from 'next/link';
 import LogoWhite from '../Images/LogoWhite';
 
@@ -36,24 +34,6 @@ export default function Navbar() {
               fontSize: '1rem',
             }}
           />
-          <Link href="/autenticacion/preRegistro">
-            <Button
-              variant="outlined"
-              startIcon={<PersonAddIcon />}
-              sx={{
-                backgroundColor: '#FF8300',
-                textTransform: 'none',
-                color: 'white',
-                borderRadius: '2rem',
-                '&:hover': {
-                  background: '#FF9A33',
-                  m: 'auto',
-                },
-              }}
-            >
-              Pre-registro
-            </Button>
-          </Link>
         </Toolbar>
       </AppBar>
     </Box>
