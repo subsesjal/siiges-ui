439f834d71ee567b552f86dea585bc38cb53d907
JoeRootX <joelaaron2141@gmail.com>
2025-07-22 22:49:26 -0600

    SDT-1095(refactor): se agrego 2 ciclos escolares

--- packages/serviciosEscolares/src/Components/Inscripciones/InscripcionesForm/Modals/CicloEscolarModal.jsx
+++ packages/serviciosEscolares/src/Components/Inscripciones/InscripcionesForm/Modals/CicloEscolarModal.jsx
@@ -57,6 +57,8 @@ export default function CicloEscolarModal({
     { id: 27, nombre: '2023C' },
     { id: 28, nombre: '2024A' },
     { id: 29, nombre: '2024B' },
+    { id: 30, nombre: '2024C' },
+    { id: 31, nombre: '2025A' },
   ];
 
   const handleFormCicloOnChange = (event) => {
@@ -110,6 +112,7 @@ export default function CicloEscolarModal({
             textValue
             onChange={handleFormCicloOnChange}
           />
+
         </Grid>
         <Grid item xs={8}>
           <Input
