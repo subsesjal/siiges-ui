7b2c9c235f8a455bb0a91b8f70d5b5d3e9318142
Roberto Cervantes Torres <robert.hct@gmail.com>
2025-08-17 22:04:21 -0600

    Update index.jsx

--- apps/siiges-app/pages/serviciosEscolares/acreditacion/index.jsx
+++ apps/siiges-app/pages/serviciosEscolares/acreditacion/index.jsx
@@ -1,29 +1,32 @@
-import {
-  AcreditacionAsignaturas,
-  InscripcionForm,
-} from '@siiges-ui/serviciosescolares';
 import { Layout } from '@siiges-ui/shared';
 import React, { useState } from 'react';
+import {
+  InscripcionForm,
+  InscripcionesTable,
+} from '@siiges-ui/serviciosescolares';
 
-export default function Acreditacion() {
+export default function Inscripcion() {
   const [asignaturas, setAsignaturas] = useState();
   const [programaId, setProgramaId] = useState();
   const [grupoId, setGrupoId] = useState();
+  const [cicloTxt, setCicloTxt] = useState();
   const [loading, setLoading] = useState(true);
 
   return (
-    <Layout title="Acreditación" loading={loading}>
+    <Layout title="Inscripción" loading={loading}>
       <InscripcionForm
         setAsignaturas={setAsignaturas}
         setProgramaId={setProgramaId}
         setGrupoId={setGrupoId}
         setLoading={setLoading}
+        setCicloTxt={setCicloTxt}
       />
       {grupoId && (
-        <AcreditacionAsignaturas
+        <InscripcionesTable
           asignaturas={asignaturas}
           programaId={programaId}
           grupoId={grupoId}
+          cicloTxt={cicloTxt}
         />
       )}
     </Layout>
