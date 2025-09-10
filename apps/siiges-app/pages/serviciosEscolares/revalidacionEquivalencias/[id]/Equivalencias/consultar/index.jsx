8ac28bfbf1eaeb407351f0eb1f5acdccfb21e7a8
Luis Manuel <elmejorlui@gmail.com>
2025-09-10 15:04:53 -0600

    SDT-1146: correccion materias equivalentes en equivalencias

--- apps/siiges-app/pages/serviciosEscolares/revalidacionEquivalencias/[id]/Equivalencias/consultar/index.jsx
+++ apps/siiges-app/pages/serviciosEscolares/revalidacionEquivalencias/[id]/Equivalencias/consultar/index.jsx
@@ -1,15 +1,14 @@
 import { Grid } from '@mui/system';
 import { ConsultEquivalencia } from '@siiges-ui/revalidaciones';
 import { Layout } from '@siiges-ui/shared';
-import React, { useState } from 'react';
+import React from 'react';
 
 export default function ConsultarEquivalencia() {
-  const [estatus, setEstatus] = useState({ estatus: null });
   return (
     <Layout title="Consultar Solicitud de Equivalencias">
       <Grid container>
         <Grid item xs={12}>
-          <ConsultEquivalencia estatus={estatus} setEstatus={setEstatus} />
+          <ConsultEquivalencia />
         </Grid>
       </Grid>
     </Layout>
