5304e69c25a0af2c0638f7313058e6ea5115eec3
JoeRootX <joelaaron2141@gmail.com>
2025-10-14 17:03:59 -0600

    SDT-1264: setNoti solo se dispara si realmente hay un problema después de que los datos estén disponibles

--- packages/serviciosEscolares/src/Components/Folios/FoliosForm/index.jsx
+++ packages/serviciosEscolares/src/Components/Folios/FoliosForm/index.jsx
@@ -58,6 +58,8 @@ export default function FoliosForm({
     const asignarInstitucionDesdeSesion = async () => {
       if (isAdmin) return;
 
+      if (!instituciones || instituciones.length === 0 || !session) return;
+
       const institucionId = await getInstitucionIdFromSession({
         instituciones,
         session,
