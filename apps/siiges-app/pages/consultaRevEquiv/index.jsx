73548d1696876e7157e32a65302bd07d8c0bc280
Luis Manuel <elmejorlui@gmail.com>
2025-12-15 16:14:45 -0600

    SDT-1400: consultar revEquiv publico por folio

---
+++ apps/siiges-app/pages/consultaRevEquiv/index.jsx
@@ -0,0 +1,11 @@
+import { Layout } from '@siiges-ui/shared';
+import { ConsultPublic } from '@siiges-ui/revalidaciones';
+import React from 'react';
+
+export default function ConsultaRevEquiv() {
+  return (
+    <Layout title="Consulta Revalidacion/Equivalencia">
+      <ConsultPublic />
+    </Layout>
+  );
+}
