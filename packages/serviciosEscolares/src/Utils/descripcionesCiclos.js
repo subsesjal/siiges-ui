d29be06f12e8623670b89d5d46c046ef3912acc4
Luis Manuel <elmejorlui@gmail.com>
2026-07-21 16:38:35 -0600

    SDT-1669: correccion ciclos escolares en programas

---
+++ packages/serviciosEscolares/src/Utils/descripcionesCiclos.js
@@ -0,0 +1,7 @@
+const descripcionesCiclos = [
+  { id: 1, nombre: 'SEMESTRAL' },
+  { id: 2, nombre: 'CUATRIMESTRAL' },
+  { id: 3, nombre: 'EQUIVALENTE' },
+];
+
+export default descripcionesCiclos;
