630348ef4baac5de137d2fd2134ee3669ee10605
Luis Manuel <elmejorlui@gmail.com>
2026-07-21 16:46:45 -0600

    SDT-1669: solo EQUIV

--- packages/serviciosEscolares/src/Utils/descripcionesCiclos.js
+++ packages/serviciosEscolares/src/Utils/descripcionesCiclos.js
@@ -1,7 +1,7 @@
 const descripcionesCiclos = [
   { id: 1, nombre: 'SEMESTRAL' },
   { id: 2, nombre: 'CUATRIMESTRAL' },
-  { id: 3, nombre: 'EQUIVALENTE' },
+  { id: 3, nombre: 'EQUIV' },
 ];
 
 export default descripcionesCiclos;
