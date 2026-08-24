d8b2ba1719e62c95d8c71e964843b0e42aacf3fb
elmejorlui <elmejorlui@gmail.com>
2026-08-24 12:08:42 -0600

    correccion variables de nuevo alumno

--- packages/serviciosEscolares/src/Components/Alumnos/FormAlumno/dataAlumnos.js
+++ packages/serviciosEscolares/src/Components/Alumnos/FormAlumno/dataAlumnos.js
@@ -61,7 +61,7 @@ export const cicloIngresoValidator = (ciclo) => {
 
 export const setFormData = (data) => ({
   situacionId: data?.situacionId || 2,
-  programaId: data?.programaId,
+  programaId: data?.alumnoId,
   matricula: data?.matricula,
   alumnoCicloIngreso: data?.alumnoCicloIngreso,
   estatus: 1,
