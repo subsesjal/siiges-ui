12a06c778197f9ae88cc94b62082ccec3965526b
elmejorlui <elmejorlui@gmail.com>
2026-08-26 12:16:02 -0600

    mapeado de seriacion corregido

--- packages/serviciosEscolares/src/Components/utils/AsignaturasEditModal.jsx
+++ packages/serviciosEscolares/src/Components/utils/AsignaturasEditModal.jsx
@@ -71,7 +71,10 @@ export default function AsignaturasEditModal({
 
   const handleOnChange = ({ target: { name, value } }) => {
     setFormAsignaturas((prev) => {
-      const newValue = name === 'tipo' ? Number(value) : value;
+      let newValue = value;
+      if (name === 'tipo') newValue = Number(value);
+      if (name === 'seriacion') newValue = Array.isArray(value) ? value.join(',') : value;
+
       return {
         ...prev,
         [name]: newValue,
@@ -114,7 +117,10 @@ export default function AsignaturasEditModal({
   };
 
   const seriacionOptions = useMemo(
-    () => asignaturasList.map(({ id, clave }) => ({ id, nombre: clave })),
+    () => asignaturasList.map(({ nombre, clave }) => ({
+      id: clave,
+      nombre: `${nombre} | ${clave}`,
+    })),
     [asignaturasList],
   );
 
@@ -225,12 +231,14 @@ export default function AsignaturasEditModal({
           <BasicSelect
             title="Seriación"
             name="seriacion"
-            value={formAsignaturas.seriacion || ''}
-            options={seriacionOptions}
-            disabled={isDisabled}
-            onChange={handleOnChange}
+            value={
+              formAsignaturas.seriacion
+                ? formAsignaturas.seriacion.split(',')
+                : []
+            }
             multiple
-            textValue
+            options={seriacionOptions}
+            onChange={handleOnChange}
           />
         </Grid>
         <Grid item xs={4}>
