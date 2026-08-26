c7cb8219345dd81bf3a3f108876eb8a517c96f20
elmejorlui <elmejorlui@gmail.com>
2026-08-26 11:53:20 -0600

    seriacion en asignaturas programas es multiselect

--- packages/serviciosEscolares/src/Components/utils/AsignaturasEditModal.jsx
+++ packages/serviciosEscolares/src/Components/utils/AsignaturasEditModal.jsx
@@ -229,6 +229,7 @@ export default function AsignaturasEditModal({
             options={seriacionOptions}
             disabled={isDisabled}
             onChange={handleOnChange}
+            multiple
             textValue
           />
         </Grid>
